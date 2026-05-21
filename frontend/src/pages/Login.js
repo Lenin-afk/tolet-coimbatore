74906217124060ba8176cfc4f5b081615a41fc10
Lenin <leninrithick@gmail.com>
2026-05-21 18:00:14 +0530

    Day 18: Connected Login and Register to FastAPI backend, JWT token stored in localStorage

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,21 +1,63 @@
-// TODO: Create a simple login form with username and password inputs
-// and a submit button
-function Login (){
+import { useState } from 'react';
 
-    return (
-        <div>
-            <h1>Login</h1>
-            <input 
-                type="text"
-                placeholder="Type your username"
-            />
-            <input 
-                type="password"
-                placeholder="Type your password"
-            />
-            <button>Login</button>
-        </div>
-    )
+function Login() {
+  // TODO: Add state for username and password
+  // TODO: Add state for "message" with default value ""
+  const [username,SetUsername]=useState("")
+  const [password,SetPassword]=useState("")
+  const [message,SetMessage]=useState("")
+
+  const handleLogin = async () => {
+    // TODO: POST to "http://127.0.0.1:8000/login"
+    // with body: { username, password }
+    // TODO: If response has token, save it using localStorage.setItem("token", token)
+    // TODO: Set message to "Login successful!" or response.error
+    fetch(("http://127.0.0.1:8000/login"),{
+        method:"POST",
+        headers:{"Content-Type":"application/json"},
+        body:JSON.stringify({username,password})
+    })
+    .then((response)=>{
+        return response.json()
+    })
+    .then((data)=>{
+        if (data.token) {
+            localStorage.setItem("token",data.token)
+            SetMessage("Login successful!")
+        }
+        else {
+            localStorage.removeItem("token")
+            SetMessage(data.error)
+        }
+    })
+
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+      {/* TODO: Add input for username */}
+      {/* TODO: Add input for password */}
+      {/* TODO: Add button that calls handleLogin */}
+      {/* TODO: Show message */}
+      <input 
+        type="text"
+        placeholder="Type your name here"
+        onChange={(e)=>{
+            SetUsername(e.target.value);
+        }}
+      />
+      <input 
+        type="password"
+        placeholder="Type your password here"
+        onChange={(e)=>{
+            SetPassword(e.target.value);
+        }}
+      />
+      <button onClick={handleLogin}>Login</button>
+      <p>{message}</p>
+    </div>
+  );
 }
 
 export default Login;
--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -1,21 +1,54 @@
-// TODO: Create a simple register form with username and password inputs
-// and a submit button
-function Register(){
+import { useState } from 'react';
 
-    return(
-        <div>
-            <h1>Register</h1>
-            <input 
-                type="text"
-                placeholder="Type your name"
-            />
-            <input         
-                type="password"
-                placeholder="Type your password"
-            />
-            <button>Register</button>
-        </div>
-    )
+function Register() {
+  // TODO: Add state for username and password
+  // TODO: Add state for "message" with default value ""
+  const [username, SetUsername]= useState("")
+  const [password, SetPassword]= useState("")
+  const [message, SetMessage]= useState("")
+
+  const handleRegister = async () => {
+    // TODO: POST to "http://127.0.0.1:8000/register"
+    // with body: { username, password }
+    // TODO: Get response JSON and set message to response.message or response.error
+    fetch(("http://127.0.0.1:8000/register"), {
+        method:"POST",
+        headers:{"Content-Type": "application/json"},
+        body:JSON.stringify({username,password})
+    })
+    .then((response)=>{
+        return response.json()
+    })
+    .then((data)=>{
+        SetMessage(data.message || data.error)
+    })
+  };
+
+  return (
+    <div>
+      <h2>Register</h2>
+      {/* TODO: Add input for username that updates state */}
+      {/* TODO: Add input for password that updates state */}
+      {/* TODO: Add button that calls handleRegister on click */}
+      {/* TODO: Show message below button */}
+      <input  
+        type="text"
+        placeholder="Type your name"
+        onChange={(e)=>{
+            SetUsername(e.target.value);
+        }}
+      />
+      <input 
+        type="password"
+        placeholder="Type your password"
+        onChange={(e)=>{
+            SetPassword(e.target.value);
+        }}
+      />
+      <button onClick={handleRegister}>Register</button>
+      <p>{message}</p>
+    </div>
+  );
 }
 
 export default Register;
